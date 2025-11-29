4298b99b49738b096287f1f222988751d774384f
sirius <suedaunal78@gmail.com>
2025-11-30 01:11:18 +0300

    common module
    nglf ile alakalı bişeyler galiba

--- maratonic-frontend/src/app/pages/login/login.component.ts
+++ maratonic-frontend/src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
--- maratonic-frontend/src/app/pages/register/register.component.ts
+++ maratonic-frontend/src/app/pages/register/register.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
